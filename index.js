382e0d61bf11835679f83cc2daff0ea94305a1d5
Alok Rathava <45447327+alokrathava@users.noreply.github.com>
2022-09-11 20:35:46 -0400

    Web322 - Assignment 1 Completed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Alokkumar Rathava Student ID: 145877205 Date: 09/11/2022
+ *
+ *  Online (Cyclic) URL:
+ *
+ ********************************************************************************/
+
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,7 +19,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('Alokkumar Rathava - 145877205');
 });
 
 server.listen(port, hostname, () => {
